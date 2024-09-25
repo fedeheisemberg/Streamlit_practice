32b28d0c7724fad08fbacc88c702c35bb2623ba7
Federico <fedemarti0107@gmail.com>
2024-09-25 12:56:34 -0300

    Update options_data_dashboard.py

--- options_dashboard/options_data_dashboard.py
+++ options_dashboard/options_data_dashboard.py
@@ -18,7 +18,7 @@ def get_theme():
     return st.get_option("theme.base")
 
 # Cargar logo basado en el tema
-if get_theme() == "light":
+if get_theme() == "dark":
     st.image("options_dashboard/logo2.png")
 else:
     st.image("options_dashboard/logo1.png")
